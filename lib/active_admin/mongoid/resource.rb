878d33dcd0ac8a08a427f5e1215e576feb325414
James McKinney <james@slashpoundbang.com>
2012-07-06 11:38:12 -0400

    fulfill @todo now that https://github.com/mongoid/mongoid/pull/2175 is fixed

--- lib/active_admin/mongoid/resource.rb
+++ lib/active_admin/mongoid/resource.rb
@@ -14,8 +14,7 @@ class ActiveAdmin::ResourceController
   # Use #desc and #asc for sorting.
   def sort_order(chain)
     params[:order] ||= active_admin_config.sort_order
-    # @todo remove once https://github.com/mongoid/mongoid/pull/2175 is fixed
-    if params[:order] && params[:order] != 'id_desc' && params[:order] =~ /^([\w\_\.]+)_(desc|asc)$/
+    if params[:order] && params[:order] =~ /^([\w\_\.]+)_(desc|asc)$/
       chain.send($2, $1)
     else
       chain # just return the chain
