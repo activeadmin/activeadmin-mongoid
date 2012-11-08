1fa5ea30d7f06d5eda9bd9de4059b692a511030f
Mattias Ekberg <mattias@mattiasekberg.com>
2012-11-08 21:40:37 +0100

    fixes #25 according to ivoreis comment.

--- lib/active_admin/mongoid/helpers/collection.rb
+++ lib/active_admin/mongoid/helpers/collection.rb
@@ -2,7 +2,7 @@ module ActiveAdmin
   module Helpers
     module Collection
       def collection_size(collection=collection)
-        collection.count(true)
+        collection.count()
       end
     end
   end
