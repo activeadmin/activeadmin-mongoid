7fb5729d25d44733714d7489eee8d2de2f736bc6
Elia Schito <elia@schito.me>
2013-02-19 14:44:33 +0100

    Remove specs that are useless

--- spec/support/templates/admin/stores.rb
+++
@@ -1,1 +0,0 @@
-ActiveAdmin.register Store
--- spec/unit/base_controller_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-require File.expand_path('base_controller_shared_examples', File.dirname(__FILE__))
-
-describe ActiveAdmin::BaseController do
-  let(:controller) { ActiveAdmin::BaseController.new }
-
-  it_should_behave_like "BaseController"
-end
