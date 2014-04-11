d5448213bb1d340d5ec6c1bf1b9b299a6eb18f72
Frederico Araujo <fred.the.master@gmail.com>
2014-04-11 20:10:35 +0700

    Fixed sorting in activeadmin 1.0

---
+++ lib/active_admin/mongoid/order_clause.rb
@@ -0,0 +1,21 @@
+module ActiveAdmin
+  class OrderClause
+    attr_reader :field, :order
+
+    def initialize(clause)
+      clause =~ /^([\w\_\.]+)_(desc|asc)$/
+      @column = $1
+      @order = $2
+
+      @field = @column
+    end
+
+    def valid?
+      @column.present? && @order.present?
+    end
+
+    def to_sql
+      [@column, ' ', @order].compact.join
+    end
+  end
+end
