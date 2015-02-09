58af92c42574cd9199e5a79de74c77519218e0e9
Nic Boie <boie0025@gmail.com>
2015-02-08 21:47:20 -0600

    Cleanup for rb 2.2

--- lib/active_admin/mongoid/helpers/collection.rb
+++ lib/active_admin/mongoid/helpers/collection.rb
@@ -5,7 +5,8 @@ module ActiveAdmin
       alias original_collection_size collection_size
       original_collection_size = instance_method(:collection_size)
 
-      def collection_size(collection=collection)
+      def collection_size(collection = nil)
+        collection ||= self.collection
         if collection.is_a?(::Mongoid::Criteria)
           collection.count(true)
         else
