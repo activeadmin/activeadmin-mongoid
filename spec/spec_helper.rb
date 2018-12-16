edaf3adf17da405d63ffa599428f0b1da8dd94c0
David Rodríguez <deivid.rodriguez@riseup.net>
2018-12-16 12:14:40 +0100

    Fix RSpec deprecation warning
    ```
    Requiring `rspec/autorun` when running RSpec via the `rspec` command is
    deprecated. Called from
    /path/to/activesupport-5.2.2/lib/active_support/dependencies.rb:291:in
    `require'.
    ```

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,6 @@ Bundler.require
 
 require File.expand_path("../../test_app/config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
